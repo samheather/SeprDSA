b8cc713ec6cfdf03b7aea49a1386d16e1736b1e9
Dan <dt636@pc619s.cs.york.ac.uk>
2013-11-18 15:15:24 +0000

    Added ability to draw black box

--- SeprDSA/src/SeprDSA.java
+++ SeprDSA/src/SeprDSA.java
@@ -1,15 +1,30 @@
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+import org.lwjgl.LWJGLException;
 
 public class SeprDSA {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws LWJGLException  {
 		// TODO Auto-generated method stub
-		for (int i=0; i<3;i++){
+		/*for (int i=0; i<3;i++){
 		System.out.println("David wrote this line.");
 		System.out.println("Jamaal wrote this line.");
 		System.out.println("Sam wrote this line.");
 		System.out.println("Dan thinks Jamaal smells.");
-		}
+		}*/
+		
+		Display.setDisplayMode(new DisplayMode(800,600));
+		Display.create();
+		
+		while (!Display.isCloseRequested()) {
+			
+			// render OpenGL here
+			
+			Display.update();
+			}
+		
 		
 	}
 
 }
+
