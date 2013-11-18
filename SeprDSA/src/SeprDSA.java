baa337827644f1c1b34b2ce22ca243847e2ac0f2
Dan <dt636@pc619s.cs.york.ac.uk>
2013-11-18 14:16:44 +0000

    It's true

--- SeprDSA/src/SeprDSA.java
+++ SeprDSA/src/SeprDSA.java
@@ -4,7 +4,7 @@ public class SeprDSA {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Sam wrote this line.\n");
-		System.out.println("Dan the man wrote this line.\n");
+		System.out.println("Dan thinks David smells.\n");
 		System.out.println("David wrote this line.\n");
 		
 		System.out.println("Jamaal wrote this line.\n");
