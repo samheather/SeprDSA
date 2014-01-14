b1deb559936cf9046e79e1562d811749b422168a
David <dh763@york.ac.uk>
2014-01-14 18:18:32 +0000

    Added JUnit and Started testing for FuturePlane

---
+++ SeprDSA/src/testing/FuturePlaneTest.java
@@ -0,0 +1,16 @@
+package testing;
+
+import static org.junit.Assert.*;
+import game.FuturePlane;
+
+import org.junit.Test;
+
+public class FuturePlaneTest {
+
+	@Test
+	public void testGenerateFlightNumber() throws InterruptedException {
+		assertTrue("Checks that the Flight Number is equal to 8", FuturePlane.generateFlightNumber().length() == 8); 
+	}
+
+
+}
