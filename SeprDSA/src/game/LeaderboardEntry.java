fde15ec12a412b3b6e06ab3a4054bdedc1ffb838
Sam Heather <sam@heather.sh>
2014-01-14 22:18:51 +0000

    Some comments.

--- SeprDSA/src/game/LeaderboardEntry.java
+++ SeprDSA/src/game/LeaderboardEntry.java
@@ -2,26 +2,50 @@ package game;
 
 public class LeaderboardEntry implements Comparable<LeaderboardEntry> {
 
+	/*
+	* Name of person in leaderboard
+	 */
 	private String name;
+	/*
+	 * Score of this person. 
+	 */
 	private double score;
 
+	/*
+	 * Empty constructor.
+	 */
 	public LeaderboardEntry() {
 		// Do nothing.
 	}
 
+	/*
+	 * LeaderboardEntry constructor
+	 * @param String name, double score
+	 */
 	public LeaderboardEntry(String name, double score) {
 		this.name = name;
 		this.score = score;
 	}
 
+	/*
+	 * Accessor for score
+	 * @param double score
+	 */
 	public double getScore() {
 		return score;
 	}
 
+	/*
+	 * Accessor for Name
+	 * @param String name
+	 */
 	public String getName() {
 		return name;
 	}
 
+	/* 
+	 * Setter
+	 */
 	public void setScore(double score) {
 		this.score = score;
 	}
