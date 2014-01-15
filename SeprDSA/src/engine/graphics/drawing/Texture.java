927feb634e137fda908fed0aa63c298f6207f9ae
Sam Heather <sam@heather.sh>
2014-01-15 21:28:01 +0000

    Fixing Jamaal's bugs - adding comments to Physical

--- SeprDSA/src/engine/graphics/drawing/Texture.java
+++ SeprDSA/src/engine/graphics/drawing/Texture.java
@@ -35,53 +35,7 @@ public class Texture {
 
 	private final int internal;
 	private final Vector size;
-/*
-	public Texture(String text, Font font) {
-		this(drawText(text, font));
-	}
 
-	private static BufferedImage drawText(String text, Font font) {
-		double x = Math.ceil(Drawables.virtualDisplaySize().get(0));
-		double y = Math.ceil(Drawables.virtualDisplaySize().get(1));
-		BufferedImage image = new BufferedImage((int) x, (int) y,
-				BufferedImage.TYPE_INT_ARGB);
-		Graphics2D graphics = image.createGraphics();
-		graphics.setFont(font);
-
-		graphics.setRenderingHint(RenderingHints.KEY_RENDERING,
-				RenderingHints.VALUE_RENDER_QUALITY);
-
-		graphics.setRenderingHint(RenderingHints.KEY_COLOR_RENDERING,
-				RenderingHints.VALUE_COLOR_RENDER_QUALITY);
-
-		graphics.setRenderingHint(RenderingHints.KEY_ALPHA_INTERPOLATION,
-				RenderingHints.VALUE_ALPHA_INTERPOLATION_QUALITY);
-
-		graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING,
-				RenderingHints.VALUE_ANTIALIAS_ON);
-
-		graphics.setRenderingHint(RenderingHints.KEY_DITHERING,
-				RenderingHints.VALUE_DITHER_ENABLE);
-
-		graphics.setRenderingHint(RenderingHints.KEY_FRACTIONALMETRICS,
-				RenderingHints.VALUE_FRACTIONALMETRICS_ON);
-
-		graphics.setRenderingHint(RenderingHints.KEY_INTERPOLATION,
-				RenderingHints.VALUE_INTERPOLATION_BICUBIC);
-
-		graphics.setRenderingHint(RenderingHints.KEY_STROKE_CONTROL,
-				RenderingHints.VALUE_STROKE_NORMALIZE);
-
-		graphics.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
-				RenderingHints.VALUE_TEXT_ANTIALIAS_LCD_HRGB);
-
-		double sizex = graphics.getFontMetrics().stringWidth(text);
-		double sizey = graphics.getFontMetrics().getHeight();
-		graphics.drawString(text, (int) ((x / 2.0) - (sizex / 2.0)),
-				(int) ((y / 2.0) + (sizey / 2.0)));
-		return image;
-	}
-*/
 	public Texture(String filePath) throws IOException {
 		/*
 		 * BufferedImage image = null; try { image = ImageIO.read(new
