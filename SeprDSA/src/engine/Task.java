6b249086cfd7221801e6dc1254c09f856a5b23fb
Jamaal Malek <mjm540@york.ac.uk>
2014-01-16 11:55:16 +0000

    Documenting Drawables and Task, and changed the way the TWL theme is loaded (It is now loaded inside pain and passed into drawables through the call to Drawables.initialize)

--- SeprDSA/src/engine/Task.java
+++ SeprDSA/src/engine/Task.java
@@ -1,14 +1,20 @@
 package engine;
 
-
-
-class Task implements Comparable<Task>{
+/**
+ * This class represents a task that is to be executed asynchronously by
+ * engine.Timing
+ * 
+ * @author mjm540
+ * 
+ */
+class Task implements Comparable<Task> {
 	public double interval;
 	public double scheduled;
 	public int times;
 	public Timing.NRunnable does;
 
-	public Task(double interval, double scheduled, int times, Timing.NRunnable does) {
+	public Task(double interval, double scheduled, int times,
+			Timing.NRunnable does) {
 		this.interval = interval;
 		this.scheduled = scheduled;
 		this.times = times;
