933c0554d5d664ea7295ad1769b21c4b302ea695
David <dh763@york.ac.uk>
2014-01-17 18:29:51 +0000

    Fixed timing test

--- SeprDSA/src/testing/TimingTest.java
+++ SeprDSA/src/testing/TimingTest.java
@@ -1,37 +1,35 @@
 package testing;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-import engine.Timing;
-import engine.TaskCanceller;
-import engine.Timing.NRunnable;
+import engine.timing.*;
+
 
 public class TimingTest {
 	
 	private int x = 0;
-	TaskCanceller tc1 = null;
-	TaskCanceller tc2 = null;
+	engine.timing.TaskCanceller tc1 = null;
+	engine.timing.TaskCanceller tc2 = null;
 	double startTime = System.currentTimeMillis();
 	
 	@Test
 	public void testLogic() {
-		tc1 = Timing.doIn(51, new NRunnable() {
+		tc1 = Timing.doIn(51, new Timing.NRunnable() {
 			public void run (int n) {
 				System.out.println("fail");
 				tc2.cancel();
 			}
-		}, null);
-		tc2 = Timing.doNTimes(10, 10, new NRunnable() {
+		});
+		tc2 = Timing.doNTimes(10, 10, new Timing.NRunnable() {
 			public void run (int n) {
 				System.out.println("win");
 				//System.out.println(tc2.cancel.value);
 			}
-		}, null);
+		});
 		//tc1.cancel();
 		while((System.currentTimeMillis() - startTime) <= 1000) {
-			Timing.logic();
+			engine.timing.Timing.logic();
 		}
 		
 	}
