601c232fa84ef263aa3595da9097374ee36fea22
tapuu <mjm540@york.ac.uk>
2014-01-19 00:13:01 +0000

    Added better error messgae for loading images

--- SeprDSA/src/game/Images.java
+++ SeprDSA/src/game/Images.java
@@ -7,8 +7,9 @@ import engine.graphics.drawing.Texture;
 /**
  * Images class - used to define the images used within the projec to save
  * writing file paths elsewhere.
+ * 
  * @author sbh514
- *
+ * 
  */
 
 public class Images {
@@ -17,17 +18,22 @@ public class Images {
 	 * <p>
 	 * Calls the creation of a texture from an image from the Texture class.
 	 * Throws IOException if image not found.
+	 * 
 	 * @param fileName
 	 * @return
 	 */
+	private static final String imagesLocation = "res/images";
+
 	private static Texture load(String fileName) {
 		// Initially null before changing.
 		Texture i = null;
 		try {
 			// Get the path by concatenating relative path with fileName
-			i = new Texture("res/images/" + fileName);
+			i = new Texture(imagesLocation + "/" + fileName);
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
+			System.out.println("Could not load image at: '" + imagesLocation
+					+ "/" + fileName + "'");
 			e.printStackTrace();
 			System.exit(0);
 		}
@@ -42,11 +48,9 @@ public class Images {
 	/**
 	 * Array of plane textures (to save creating multiple un-ordered objects.
 	 */
-	public static Texture planes[] = new Texture[] { 
-		load("many_planes/nPlane1.png"),
-		load("many_planes/nPlane2.png"),
-		load("many_planes/nPlane3.png") };
-	public static Texture waypoints[] = new Texture[] { 
-		load("checkpoint.png")};
+	public static Texture planes[] = new Texture[] {
+			load("many_planes/nPlane1.png"), load("many_planes/nPlane2.png"),
+			load("many_planes/nPlane3.png") };
+	public static Texture waypoints[] = new Texture[] { load("checkpoint.png") };
 	public static Texture entryExitPoint = load("exit.png");
 }
