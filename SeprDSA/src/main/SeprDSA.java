944e5790b7efc9cb0d3ae0240ce7e6133cfb06d7
Sam Heather <sam@heather.sh>
2014-01-21 22:05:08 +0000

    lol

--- SeprDSA/src/main/SeprDSA.java
+++ SeprDSA/src/main/SeprDSA.java
@@ -158,7 +158,7 @@ public class SeprDSA {
 				Planes.updateTimer();
 				Physicals.logic();
 			}
-			System.out.println(1000.0/engine.timing.Timing.timeSinceLastFrame());
+			//System.out.println(1000.0/engine.timing.Timing.timeSinceLastFrame());
 			Input.logic();
 			Drawables.logic();
 			s.drawSidemenu();
