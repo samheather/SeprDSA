f749148cee32f11533375bb4dc6784825921a0bf
Jamaal Malek <mjm540@york.ac.uk>
2014-01-21 20:45:31 +0000

    Documentation

--- SeprDSA/src/engine/timing/Timing.java
+++ SeprDSA/src/engine/timing/Timing.java
@@ -10,25 +10,38 @@ public class Timing {
 	private static long lastTimeSinceLastFrame = 0;
 	private static PriorityQueue<Task> tasks = new PriorityQueue<Task>();
 
+	/**
+	 * Implemented by a class which represents a closure that is to be called
+	 * more than once.
+	 */
 	public interface NRunnable {
+		/**
+		 * Call the closure once.
+		 * 
+		 * @param n
+		 *            n decrements each time the closure is called
+		 */
 		public void run(int n);
 	}
 
 	public static TaskCanceller doIn(double milliseconds, NRunnable does) {
 		return doNTimes(1, milliseconds, does);
 	}
-	
+
 	public static void clearAllTasks() {
 		tasks.clear();
 	}
 
-	
 	/**
 	 * 
-	 * @param n Number of times to repeat 'does'
-	 * @param milliseconds Interval between iterations of 'does' in milliseconds
-	 * @param does Subroutine that performs n times
-	 * @return TaskCanceller class that is used internally to check for cancellations of tasks
+	 * @param n
+	 *            Number of times to repeat 'does'
+	 * @param milliseconds
+	 *            Interval between iterations of 'does' in milliseconds
+	 * @param does
+	 *            Subroutine that performs n times
+	 * @return TaskCanceller class that is used internally to check for
+	 *         cancellations of tasks
 	 */
 	public static TaskCanceller doNTimes(int n, double milliseconds,
 			NRunnable does) {
@@ -40,10 +53,14 @@ public class Timing {
 
 	/**
 	 * 
-	 * @param n Number of times to repeat 'does'
-	 * @param milliseconds Time between each iteration of 'does' in milliseconds
-	 * @param does Subroutine that performs n times
-	 * @param c Class used to cancel the scheduled task
+	 * @param n
+	 *            Number of times to repeat 'does'
+	 * @param milliseconds
+	 *            Time between each iteration of 'does' in milliseconds
+	 * @param does
+	 *            Subroutine that performs n times
+	 * @param c
+	 *            Class used to cancel the scheduled task
 	 */
 	public static void doNTimes(int n, double milliseconds, NRunnable does,
 			TaskCanceller c) {
@@ -60,9 +77,9 @@ public class Timing {
 		return timeSinceStart;
 	}
 
-	
 	/**
-	 *  (Needs to run every frame) Will schedule and perform tasks based on a PriorityQueue
+	 * (Needs to run every frame) Will schedule and perform tasks based on a
+	 * PriorityQueue
 	 */
 	public static void logic() {
 		timeSinceStart = System.currentTimeMillis() - startTime;
