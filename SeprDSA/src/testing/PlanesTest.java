a1c6000a26c24562464f1c013befbeee92a192ff
David <dh763@york.ac.uk>
2014-01-21 19:28:35 +0000

    Cleaned up testing

--- SeprDSA/src/testing/PlanesTest.java
+++ SeprDSA/src/testing/PlanesTest.java
@@ -10,7 +10,6 @@ import game.WayPoint;
 import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
-import java.util.ArrayList;
 
 import main.SeprDSA;
 
@@ -19,27 +18,26 @@ import org.junit.Test;
 import org.la4j.vector.dense.BasicVector;
 
 public class PlanesTest {
-	
-	@BeforeClass
-	public static void setUp() throws MalformedURLException, IOException {
-		Drawables.initialise(new Window(1024, 640), 824, 640, new File(
-				"default.xml").toURI().toURL());
-		EntryExitPoint newExit1 = new EntryExitPoint(new BasicVector(
-			new double[] { Sidemenu.width / 2,
-					Drawables.virtualDisplaySize().get(1) / 2, 11 } 
-			), -90, 85, 1); 
-		SeprDSA.addEntryExitPoint(newExit1);
-		WayPoint testWP1 = new WayPoint(
-				new BasicVector(new double[] { 10, 20, 0 }), "1");
-		WayPoint testWP2 = new WayPoint(
-				new BasicVector(new double[] { 310, 320, 0 }), "2");
-		SeprDSA.addWayPoint(testWP1);
-		SeprDSA.addWayPoint(testWP2);
-	}
-		
+//	@BeforeClass 
+//    public static void setUpClass() throws MalformedURLException, IOException {      
+//	 Drawables.initialise(new Window(1024, 640), 824, 640, new File(
+//				"default.xml").toURI().toURL());
+//}		
 	
 	@Test
 	public void testUpdateTimer() {
+		EntryExitPoint newExit1 = new EntryExitPoint(new BasicVector(
+				new double[] { Sidemenu.width / 2,
+						Drawables.virtualDisplaySize().get(1) / 2, 11 } 
+				), -90, 85, 1); 
+			SeprDSA.addEntryExitPoint(newExit1);
+			WayPoint testWP1 = new WayPoint(
+					new BasicVector(new double[] { 10, 20, 0 }), "1");
+			WayPoint testWP2 = new WayPoint(
+					new BasicVector(new double[] { 310, 320, 0 }), "2");
+			SeprDSA.addWayPoint(testWP1);
+			SeprDSA.addWayPoint(testWP2);
+			
 		game.Planes.updateTimer();
 		assertNotNull("Checks if the futurePlane array is null. Which it shouldn't be.", game.FuturePlanes.futurePlanes);
 	}
--- SeprDSA/src/testing/WaypointTest.java
+++ SeprDSA/src/testing/WaypointTest.java
@@ -2,15 +2,26 @@ package testing;
 
 import static org.junit.Assert.*;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
+import engine.graphics.Drawables;
+import engine.graphics.display.Window;
 import game.WayPoint;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.la4j.vector.dense.BasicVector;
 
 public class WaypointTest {
 
+//	@BeforeClass 
+//  public static void setUpClass() throws MalformedURLException, IOException {      
+//	 Drawables.initialise(new Window(1024, 640), 824, 640, new File(
+//				"default.xml").toURI().toURL());
+//}
+	
 	@Test
 	public void testToString() throws MalformedURLException, IOException {
 		
